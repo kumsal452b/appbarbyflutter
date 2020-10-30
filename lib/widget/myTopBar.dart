8502b8e9309453b95cc99950e5bd1206fc22ef85
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-30 19:29:01 +0300

    error not found yet

---
+++ lib/widget/myTopBar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class deneme{
+  
+}
+class mytopappbar extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
