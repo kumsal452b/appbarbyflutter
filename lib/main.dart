3292d003c8ff6307a5ca62620c8a04e09f0d6f08
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-30 16:41:11 +0300

    error notfound yet

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,8 @@ void main() {
 
   runApp(
     new CupertinoApp(
-      home: MyApp(),
-      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-        DefaultMaterialLocalizations.delegate,
-        DefaultWidgetsLocalizations.delegate,
-      ],
+      home: MyHomePage(),
+
     )
   );
 }
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-
-    );
-  }
-}
-
-
--- lib/widget/bottomNavigate.dart
+++ lib/widget/bottomNavigate.dart
@@ -1,27 +1,18 @@
-import 'package:appbarbyflutter/main.dart';
-import 'package:appbarbyflutter/widget/firstTab.dart';
+import 'package:appbarbyflutter/screens/MyHomePage.dart';
 import 'package:appbarbyflutter/widget/seccondTab.dart';
 import 'package:appbarbyflutter/widget/thirsTab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'firstTab.dart';
 final GlobalKey<NavigatorState> firstTabNavKey = GlobalKey<NavigatorState>();
 final GlobalKey<NavigatorState> secondTabNavKey = GlobalKey<NavigatorState>();
 final GlobalKey<NavigatorState> thirdTabNavKey = GlobalKey<NavigatorState>();
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class bottomTabView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-   return CupertinoTabScaffold(
+    return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         items: [
           BottomNavigationBarItem(
@@ -39,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-     tabBuilder: (contex,index){
+      tabBuilder: (contex,index){
         if(index==0){
           return CupertinoTabView(
             navigatorKey: firstTabNavKey,
@@ -58,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
             builder: (context)=>thirsTab(),
           );
         }
-     },
-   );
+      },
+    );
   }
 }
