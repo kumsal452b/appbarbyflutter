e081cd401e1ef04983e69cef4298acf5dbdbf21a
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-31 17:24:16 +0300

    add new chıldern element

--- lib/screens/vehicles.dart
+++ lib/screens/vehicles.dart
@@ -10,14 +10,32 @@ class _vehiclesState extends State<vehicles> {
   ListView listView=new ListView();
   @override
   void initState() {
-    listView.
+
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: listView.
+      child: ListView(
+      children: <Widget>[
+          Container(
+            height: 50,
+            color: Colors.amber[600],
+            child: const Center(child: Text('Entry A')),
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[500],
+            child: const Center(child: Text('Entry B')),
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[100],
+            child: const Center(child: Text('Entry C')),
+          ),
+        ],
+      )
     );
   }
 }
