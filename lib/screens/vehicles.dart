1ad801d15890d056730ec612dea453459c470499
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-31 17:08:56 +0300

    add new wehıcles screen

---
+++ lib/screens/vehicles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class vehicles extends StatefulWidget {
+  @override
+  _vehiclesState createState() => _vehiclesState();
+}
+
+class _vehiclesState extends State<vehicles> {
+  ListView listView=new ListView();
+  @override
+  void initState() {
+    listView.
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: listView.
+    );
+  }
+}
